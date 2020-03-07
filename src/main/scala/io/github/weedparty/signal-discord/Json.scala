828075ff87e920249f37917417e087835dc80d29
Flasushi <flash.osushi@gmail.com>
2020-03-07 16:06:56 +0900

    #4 json4sでjsonをパースするやつをした

---
+++ src/main/scala/io/github/weedparty/signal-discord/Json.scala
@@ -0,0 +1,15 @@
+package io.github.weedparty.signaldiscord
+
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+
+import scala.io.Source
+
+class Json {
+  def json4s(): Map[String, Any] = {
+    val source = Source.fromFile("config/config.json", "UTF-8")
+    val json = parse(source.mkString)
+    val maps = json.values.asInstanceOf[Map[String, Any]]
+    maps
+  }
+}
--- src/main/scala/io/github/weedparty/signal-discord/Main.scala
+++ src/main/scala/io/github/weedparty/signal-discord/Main.scala
@@ -1,7 +1,15 @@
 package io.github.weedparty.signaldiscord
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("ohaYO")
-  }
+import ackcord._
+import ackcord.data._
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+
+case class Configure(clientId: String, clientSecret: String){
+  implicit val formats = DefaultFormats
+}
+
+object Main extends Json with App {
+  val key = json4s().get("clientId") //clientId or clientSecret
+  println(key)
 }
