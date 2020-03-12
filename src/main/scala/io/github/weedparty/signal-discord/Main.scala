5705c63661431f6234a8739207dc1ffff38e650e
Flasushi <flash.osushi@gmail.com>
2020-03-12 22:58:41 +0900

    #4 @ekuinox の助けでpingpongできるようになった

--- src/main/scala/io/github/weedparty/signal-discord/Main.scala
+++ src/main/scala/io/github/weedparty/signal-discord/Main.scala
@@ -1,11 +1,33 @@
 package io.github.weedparty.signaldiscord
 
 import ackcord._
-import ackcord.data._
-import org.json4s._
-import org.json4s.jackson.JsonMethods._
+import ackcord.syntax._
 
 object Main extends App {
-  val key = Json.json4s.get("clientId") //clientId or clientSecret
-  println(key)
+
+  require(Json.json4s.contains("token"))
+  val token = Json.json4s("token").asInstanceOf[String]
+  val settings = ClientSettings(token)
+  import settings.executionContext
+
+  settings
+    .createClient()
+    .foreach { client =>
+      client.onEvent {
+        import client.sourceRequesterRunner._
+        client.withCache[SourceRequest, APIMessage] { implicit c => {
+          case APIMessage.Ready(_) =>
+            println("ready")
+            client.sourceRequesterRunner.unit
+          case APIMessage.MessageCreate(message, _) if message.content == "ping" && message.isAuthorUser =>
+            for {
+              guildChannel <- optionPure(message.tGuildChannel)
+              _ <- run(guildChannel.sendMessage("pong"))
+            } yield ()
+          case _ => client.sourceRequesterRunner.unit
+        }
+        }
+      }
+      client.login()
+    }
 }
